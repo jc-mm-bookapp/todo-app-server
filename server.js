d67b3ea93a4f9e4d7ad13a458dc8d878e085c340
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-07-26 13:34:14 -0700

    added singlepage functionality

--- server.js
+++ server.js
@@ -20,24 +20,23 @@ app.get('/api/v1/books', (req, res) => {
     .catch(console.error);
 });
 
-  //--------------------------------------------------------------
-  //--------------------------------------------------------------
+  // --------------------------------------------------------------
+  // --------------------------------------------------------------
 
-// app.get('/api/v1/books/:id', (req, res) =>{
-//   let SQL =';';
-//   let values = [
-//     req.params.id
-//   ];
-//   client.query(SQL, values)
-//     .then(results => res.send(results.rows))
-//     .catch(console.error);
+app.get('/api/v1/books/:id', (req, res) =>{
+  let SQL ='SELECT book_id, title, author, image_url, isbn, book_description FROM books WHERE book_id = $1;';
+  let values = [
+    req.params.id
+  ];
+  client.query(SQL, values)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
 
-// })
+})
 
 // app.post('/api/v1/books', (req, res) =>{
-//   let SQL =';';
+//   let SQL ='INSERT INTO books (title, author, image_url, isbn, book_description) VALUES ($1, $2, $3, $4, $5);';
 //   let values = [
-//     req.params.id,
 //     req.body.title,
 //     req.body.author,
 //     req.body.image_url,
@@ -49,8 +48,8 @@ app.get('/api/v1/books', (req, res) => {
 //     .catch(console.error);
 // })
 
-  //--------------------------------------------------------------
-  //--------------------------------------------------------------
+  // --------------------------------------------------------------
+  // --------------------------------------------------------------
 
 app.get('*', (req, res) => {
   res.status(404).send('This route does not exist: 01 for a test message');
